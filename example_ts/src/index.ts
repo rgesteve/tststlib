64a6f60ec769222a5f3db5f435b4e65fd3855b1b
R. G. Esteves <rgesteves@gmail.com>
2018-10-30 07:39:33 -0700

    Importing the library

--- example_ts/src/index.ts
+++ example_ts/src/index.ts
@@ -1,1 +1,6 @@
-console.log("Testing from typescript")
+import * as tststlib from 'tststlib'; // vscode complains, but webpack finds this just fine
+
+console.log("Testing from typescript");
+let r = new tststlib.Rect(8, 9);
+console.log(`Inside typescript the rect is ${r.y} for the y coordinate`);
+console.log("Done in TS!!!");
